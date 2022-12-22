ed6b53de620a0e6025f46ab7dfa664493e53d079
quangmta <nguyennhuquang1998@gmail.com>
2022-12-22 08:30:03 +0300

    lab3

--- src/tb/scr1_tb_log_cmd.sv
+++ src/tb/scr1_tb_log_cmd.sv
@@ -4,7 +4,7 @@ always_ff @(posedge scr1_top_tb_ahb.i_top.i_imem_ahb.clk) begin
     if (scr1_top_tb_ahb.i_top.i_imem_ahb.imem_resp == 2'b01) begin
         // valid data from ahb router
         if (
-            (scr1_top_tb_ahb.i_top.i_imem_ahb.imem_rdata[31 : 0] == 32'h057E4505)
+            (scr1_top_tb_ahb.i_top.i_imem_ahb.imem_rdata[31 : 0] == 32'h1DD19963)
         ) begin
             // detect and command
             $display("PC: %04h",scr1_top_tb_ahb.i_top.i_core_top.i_pipe_top.curr_pc);
